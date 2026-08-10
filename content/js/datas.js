1d6c38ff674286f2e6a3689130255933c999c3c9
rabewarmor <145297327+rabewarmor@users.noreply.github.com>
2026-08-09 21:11:25 -0600

    Create datas.js with personal and professional info
    Added personal information, education, work experience, and projects data objects.

---
+++ content/js/datas.js
@@ -0,0 +1,45 @@
+// Created for data objects 
+
+
+
+const Personal_Info = {
+    name: "Rabewar Moradi",
+    phoneNumber: "780-938-1597", 
+    email: "rabewar@ualberta.ca",  
+}
+
+
+
+// gerneric objects for life things 
+const Education = [
+    {
+        title: "BSc Cmptr Eng Software Co-op — University of Alberta",
+        date: "Expected 2027"
+    }
+];
+
+
+const Work_Experience = [
+    {
+        // Agropur 1st 
+        title: "IT Plant Technician Intern — Agropur",
+        location: "Edmonton, Alberta (January-April)\n & Woodstock, Ontatrio (April-August)",
+        date_start: "January 2025",
+        date_end: "August 2025",
+        // bullets: [
+        // "Action + tool + outcome.",
+        // "Impact-focused bullet."
+        // ]
+    }
+];
+
+
+const projects = [
+  {
+    title: "SCADA Data Historian Temperature and Humidity Logger",
+    date: "2025",
+    // bullets: ["What you built and why it matters."]
+  }
+];
+
+
